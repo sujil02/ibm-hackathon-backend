d22790451e19a2d4dbb29b8eeb2e5f23c7616e0a
Sujil Shah <sujil@Sujils-MacBook-Pro.local>
2020-07-14 14:19:57 -0700

    basic java spring skeleton

---
+++ src/test/java/com/example/ibmhackathonbackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibmhackathonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
