d08b0cd6af3c88ac8be4d89b3d24ccdbb841524d
Sujil Shah <sujil@Sujils-MacBook-Pro.local>
2020-07-16 07:58:56 -0700

    adding post mapping

--- src/main/java/com/example/ibmhackathonbackend/controller/PostController.java
+++ src/main/java/com/example/ibmhackathonbackend/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController; 
 
@@ -22,6 +23,12 @@ public class PostController {
     @GetMapping("/api/posts")
     public Iterable<Post> findAllPosts() {
         return postService.findAllPosts();
+    }
+
+    @PostMapping("/api/post")
+    public Post createPost(@RequestBody Post post){
+        return postService.createPost(post);
+        
     }
     
 }
